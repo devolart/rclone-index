d13d8851f759cbe3ede3f62e99facbad6c3c1ef1
devolart <125961492+devolart@users.noreply.github.com>
2024-02-16 10:02:00 +0000

    Fix ugly url path

--- docker.sh
+++ docker.sh
@@ -8,6 +8,22 @@ fi
 if [ -n "${CONFIG_URL}" ]; then
     echo "Rclone config URL found"
     curl $CONFIG_URL > rclone.conf
+    
+    contents=$(cat rclone.conf)
+
+    if ! echo "$contents" | grep -q "\[combine\]"; then
+        remotes=$(echo "$contents" | grep '^\[' | sed 's/\[\(.*\)\]/\1/g')
+
+        upstreams=""
+        for remote in $remotes; do
+            upstreams+="$remote=$remote: "
+        done
+
+        upstreams=${upstreams::-1}
+
+        echo -e "\n\n[combine]\ntype = combine\nupstreams = $upstreams" >> rclone.conf
+    fi
+
 else
     echo "No Rclone config URL found, serving blank config"
     touch rclone.conf
@@ -16,7 +32,7 @@ fi
 echo "Running rclone index"
 
 if [ -n "${USERNAME}" ] && [ -n "${PASSWORD}" ]; then
-    rclone rcd --rc-serve --rc-addr=:$PORT --rc-user="$USERNAME" --rc-pass="$PASSWORD" --config rclone.conf
+    rclone serve http combine: --addr=:$PORT --user="$USERNAME" --pass="$PASSWORD" --read-only --config rclone.conf
 else
-    rclone rcd --rc-serve --rc-addr=:$PORT --config rclone.conf
+    rclone serve http combine: --addr=:$PORT --read-only --config rclone.conf
 fi
